0f5e692117f2d65e529c4f5d3235a88ab0931bf5
Gilbert Doan <gddoan@corn26.stanford.edu>
2015-02-25 15:17:45 -0800

    Bleu Evaluator Running

--- BleuScoreEvaluator.py
+++ BleuScoreEvaluator.py
@@ -1,5 +1,6 @@
 import sys
 import os.path as op
+import collections
 
 def unicount(sent):
     res = dict()
@@ -24,42 +25,56 @@ def bicount(sent):
 
     return res
 
-def bleu_for_one(ref, evl):
-    ref = ref.lower().split()
-    evl = evl.lower().split()
-
-    N = len(evl)
-    N_ref = len(ref)
-
-    if N_ref == 0:
-        return None, None
-
-    if N == 0:
-        return 0.0, 0.0
-
-    ref_unicount = unicount(ref)
-    eval_unicount = unicount(evl)
-    ref_bicount = bicount(ref)
-    eval_bicount = bicount(evl)
-
+def bleu_for_n_ref(ref_list, evl):
     unicorrect = 0
     bicorrect = 0
+    
+    evl = evl.lower().split()
+    N = len(evl)
+
+    if N == 0:
+        return None
+
+    ref_dicts = []
+    all_ref_dict_uni = collections.defaultdict(lambda:0)
+    all_ref_dict_bi = collections.defaultdict(lambda:0)
+
+    min_ref_len = float("+inf")
+    for ref in ref_list:
+        ref = ref.lower().split()
+
+        N_ref = len(ref)
+        if N_ref == 0:
+            pass
+
+        if N_ref < min_ref_len:
+            min_ref_len = N_ref
+
+        ref_unicount = unicount(ref)
+        eval_unicount = unicount(evl)
+        ref_bicount = bicount(ref)
+        eval_bicount = bicount(evl)
+
+        for word in ref_unicount:
+            all_ref_dict_uni[word] += 1
+        for word in ref_bicount:
+            all_ref_dict_bi[word] += 1
 
     for unigram in eval_unicount:
-        if unigram in ref_unicount:
-            unicorrect += min(eval_unicount[unigram], ref_unicount[unigram])
+        if unigram in all_ref_dict_uni:
+            unicorrect += min(eval_unicount[unigram], all_ref_dict_uni[unigram])
 
     for bigram in eval_bicount:
-        if bigram in ref_bicount:
-            bicorrect += min(eval_bicount[bigram], ref_bicount[bigram])
+        if bigram in all_ref_dict_bi:
+            bicorrect += min(eval_bicount[bigram], all_ref_dict_bi[bigram])
 
-    brievity = min(1.0, N * 1.0 / N_ref)
+    brievity = min(1.0, N * 1.0 / min_ref_len)
     uniprec = 1.0 * unicorrect / N
     if N > 1:
         biprec = 1.0 * bicorrect / (N-1)
     else:
         biprec = 1.0
-    
+        
     return 100.0 * brievity * uniprec, 100.0 * brievity * uniprec * biprec
 
 if __name__ == '__main__':
@@ -83,17 +98,39 @@ if __name__ == '__main__':
             n_sent = 0
             bleu1 = 0.0
             bleu2 = 0.0
+
+            list_of_translations = []
+            three_translations = []
             for ref_line in f_ref:
+                if "*" in ref_line:
+                    list_of_translations.append(three_translations)
+                    three_translations = []
+                
+                elif "*" not in ref_line:
+                    ref_line = ref_line.replace("\n", "")
+                    three_translations.append(ref_line)
+                
+
+            for i in xrange(len(list_of_translations)):
                 eval_line = f_eval.readline()
+                # list_of_originals.append(eval_line)
                 if len(eval_line) == 0:
                     print "Error: The output file should at least have the same number of sentences as the reference file."
                     sys.exit(0)
 
-                t1, t2 = bleu_for_one(ref_line, eval_line)
+                t1, t2 = bleu_for_n_ref(list_of_translations[i], eval_line)
                 if t1 is not None:
                     bleu1 += t1
                     bleu2 += t2
                     n_sent += 1
 
-    print "%d sentences evaluated.\nBLEU-1 score: %3.6f\nBLEU-2 score: %3.6f" \
-        % (n_sent, bleu1 / n_sent, bleu2 / n_sent)
+                print "%d sentences evaluated.\n" \
+                % (n_sent)
+                print "Evaluation Sentence: " + eval_line
+                print "Reference Sentences:"
+                for translation in list_of_translations[i]:
+                    print translation
+                print
+                print "BLEU-1 score: %3.6f\nBLEU-2 score: %3.6f" \
+                % (bleu1 / n_sent, bleu2 / n_sent)
+                print
